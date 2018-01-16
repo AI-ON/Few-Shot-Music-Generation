346804fac46ef66e7607734c3a1367586091c07c
Benjamin Bastian <benjamin.bastian@gmail.com>
2018-01-16 13:37:57 -0800

    Add episode sampler for lyrics data
    This commit adds an episode sampler for the lyrics data as a class
    EpisodeSampler. The EpisodeSampler splits the dataset into three subsets
    for training, validation, and testing. The information for the split can
    be persisted and loaded for future runs with the same train/val/test
    split.
    
    Each episode randomly samples a batch of artists from the dataset. For
    each artist, query and support batches of songs are sampled from the
    dataset. If an artist does not have enough songs to fill the query and
    support batches, they are skipped.
    
    The lyrics are read from a directory structure following the pattern
    `./[root dir]/[artist]/[song]`. After a song has been read, it is parsed
    into a series of "words" or "tokens" (currently just by splitting on
    whitespace). Each word is assigned an integer ID (which is optionally
    persisted to a file). The sequence of word IDs is then transformed into
    a numpy array, of a configurable length (zero-padded if the song is
    shorter than the configured length).
    
    As each song is numpy-ified, they are (optionally) cached in memory.
    From benchmarks on my laptop, I've seen that this caching offers an
    order of magnitude performance improvement for each call to
    EpisodeSampler.get_episode().

---
+++ src/data/dataset.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+"""A dataset class lyrics and MIDI data for the few-shot-music-gen project
+"""
+import os
+
+import numpy as np
+
+
+class Dataset(object):
+    """A class for train/val/test sets.
+
+    This class is initialized with the following arguments:
+    Arguments:
+        root (str): the root directory of the dataset
+        split ("train", "val", or "test"): the split of the dataset which
+            this object represents.
+        loader (LyricsLoader or MIDILoader): the object used for reading and
+            parsing
+        split_proportions (tuple of three numbers): the unnormalized
+            (train, val, test) split.
+        persist (bool): persists the train/val/test split information in csv
+            files in `root`, so future runs will use the same splits. If those
+            csvs already exist, the sampler uses the splits from those files.
+    """
+    def __init__(self, root, split, loader, split_proportions=(8,1,1),
+            persist=True, cache=True):
+        self.root = root
+        self.cache = cache
+        self.cache_data = {}
+        self.loader = loader
+        split_csv_path = os.path.join(root, '%s.csv' % split)
+        if persist and os.path.exists(split_csv_path):
+            split_csv = open(split_csv_path, 'r')
+            self.artists = [line.strip() for line in split_csv.readlines()]
+            split_csv.close()
+        else:
+            dirs = []
+            for artist in os.listdir(root):
+                if os.path.isdir(os.path.join(root, artist)):
+                    dirs.append(artist)
+            artists = []
+            skipped_count = 0
+            for artist in dirs:
+                songs = os.listdir(os.path.join(root, artist))
+                songs = [song for song in songs if not os.path.isdir(song)]
+                if len(songs) >= support_size + query_size:
+                    artist_obj = Artist(name=artist, songs=songs)
+                    artists.append(artist_obj)
+                else:
+                    skipped_count += 1
+            if skipped_count > 0:
+                print("%s artists don't have K+K'=%s songs. Using %s artists" % (
+                    skipped_count, support_size + query_size, len(artists)))
+            artist_proportion = sum(split_proportions) * len(artists)
+            train_count = int(float(split_proportions[0]) / artist_proportion)
+            val_count = int(float(split_proportions[1]) / artist_proportion)
+            np.random.shuffle(artists)
+            if persist:
+                train_csv = open(os.path.join(root, 'train.csv'), 'w')
+                val_csv = open(os.path.join(root, 'val.csv'), 'w')
+                test_csv = open(os.path.join(root, 'test.csv'), 'w')
+                train_csv.write('\n'.join(artists[:train_count]))
+                val_csv.write('\n'.join(artists[train_count:train_count+val_count]))
+                test_csv.write('\n'.join(artists[train_count+val_count:]))
+                train_csv.close()
+                val_csv.close()
+                test_csv.close()
+            if split == 'train':
+                self.artists = artists[:train_count]
+            elif split == 'val':
+                self.artists = artists[train_count:train_count+val_count]
+            else:
+                self.artists = artists[train_count+val_count:]
+
+    def load(self, song, artist):
+        if self.cache and (song, artist) in self.cache_data:
+            return self.cache_data[(song, artist)]
+        else:
+            data = self.loader(os.path.join(self.root, song, artist))
+            self.cache_data[(song, artist)] = data
+            return data
+
+    def __len__(self):
+        return len(self.artists)
+
+    def __getitem__(self, index):
+        return self.artists[index]
---
+++ src/data/episode.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+import os
+import time
+import numpy as np
+
+
+class Episode(object):
+    def __init__(self, support, query):
+        self.support = support
+        self.query = query
+
+
+class SQSampler(object):
+    """A sampler for randomly sampling support/query sets.
+
+    Arguments:
+        root (str): the root of the data directory
+        support_size (int): number of songs in the support set
+        query_size (int): number of songs in the query set
+        cache (bool): caches the song names instead of hitting the FS
+    """
+    def __init__(self, root, support_size, query_size, cache=True):
+        self.root = root
+        self.support_size = support_size
+        self.query_size = query_size
+        self.cache = cache
+        if cache:
+            self.songs = {}
+
+    def sample(self, artist):
+        if self.cache and artist in self.songs:
+            songs = self.songs[artist]
+        else:
+            artist_path = os.path.join(root, artist)
+            songs = os.listdir(artist_path)
+            songs = [song for song in songs if not os.path.isdir(song)]
+            if self.cache:
+                self.songs[artist] = songs
+        sample = np.random.choice(
+            songs,
+            size=self.support_size+self.query_size,
+            replace=False)
+        query = sample[:self.query_size]
+        support = sample[self.query_size:]
+        return query, support
+
+
+class EpisodeSampler(object):
+    def __init__(self, dataset, batch_size, support_size, query_size, max_len):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.support_size = support_size
+        self.query_size = query_size
+        self.max_len = max_len
+        self.sq_sampler = SQSampler(root, support_size, query_size)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        return 'EpisodeSampler("%s", "%s")' % (self.root, self.split)
+
+    def get_episode(self):
+        support = np.zeros((self.batch_size, self.support_size, max_len))
+        query = np.zeros((self.batch_size, self.query_size, max_len))
+        artists = np.random.choice(self.dataset, size=self.batch_size, replace=False)
+        for batch_index, artist in enumerate(artists):
+            support_songs, query_songs = self.sq_sampler.sample(artist)
+            for support_index, song in enumerate(support_songs):
+                parsed_song = self.dataset.load(artist, song)
+                support[batch_index,support_index,:] = parsed_song
+            for query_index, song in enumerate(query_songs):
+                parsed_song = self.dataset.load(artist, song)
+                support[batch_index,query_index,:] = parsed_song
+        return Episode(support, query)
