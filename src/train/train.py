cc2a8524775f2f8b3e0fd07fe04d82fcf8ea4148
Sachin Ravi <sachinravi14@gmail.com>
2018-08-09 11:14:22 -0400

    Modify training to add early stopping, tqdm to estimate model runtime, and writing log in checkpt dir

--- src/train/train.py
+++ src/train/train.py
@@ -1,12 +1,15 @@
 import os
+import sys
 import pprint
 import argparse
 import yaml
+from tqdm import tqdm
 from importlib import import_module
 
 from data.episode import load_sampler_from_config
 
 PP = pprint.PrettyPrinter(depth=6)
+LOG_FILE = 'status.log'
 
 
 def load_model_from_config(config):
@@ -33,16 +36,46 @@ def evaluate(model, episode_sampler, n_episodes):
     return avg_nll / n_episodes
 
 
+def write_samples(model, episode_sampler, samples_dir, n_samples, max_len):
+    if not os.path.exists(samples_dir):
+        os.makedirs(samples_dir)
+
+    for i in range(n_samples):
+        curr_sample_dir = os.path.join(samples_dir, 'sample_%d' % i)
+        os.makedirs(curr_sample_dir)
+
+        episode = episode_sampler.get_episode()
+        support_set = episode.support[0]
+        sample = model.sample(support_set, max_len)
+
+        for j in range(support_set.shape[0]):
+            write_seq(episode_sampler.detokenize(support_set[j]),
+                      curr_sample_dir, 'support_%d' % j)
+
+        write_seq(episode_sampler.detokenize(sample), curr_sample_dir,
+                  'model_sample')
+
+
+def create_log(checkpt_dir):
+    if checkpt_dir != '':
+        if not os.path.exists(checkpt_dir):
+            os.makedirs(checkpt_dir)
+
+        sys.stdout = open(os.path.join(checkpt_dir, LOG_FILE), 'w', 0)
+
+
 parser = argparse.ArgumentParser(description='Train a model.')
 parser.add_argument('--data', dest='data', default='')
 parser.add_argument('--model', dest='model', default='')
 parser.add_argument('--task', dest='task', default='')
 parser.add_argument('--checkpt_dir', dest='checkpt_dir', default='')
 parser.add_argument('--init_dir', dest='init_dir', default='')
+parser.add_argument('--mode', dest='mode', default='train')
 args = parser.parse_args()
 
 
 def main():
+    create_log(args.checkpt_dir)
     print('Args:')
     print(PP.pformat(vars(args)))
 
@@ -73,29 +106,57 @@ def main():
     n_samples = config['n_samples']
     max_len = config['max_len']
 
+    save_best_val = False
+    if 'patience_iters' in config:
+        save_best_val = True
+        patience_iters = config['patience_iters']
+
     model = load_model_from_config(config)
     model.recover_or_init(args.init_dir)
 
-    # Train model and evaluate
-    avg_nll = evaluate(model, episode_sampler['val'], n_val)
-    print("Iter: %d, val-nll: %.3e" % (0, avg_nll))
+    if args.mode == 'train':
+        # Train model and evaluate
+        avg_nll = evaluate(model, episode_sampler['val'], n_val)
+        print("Iter: %d, val-nll: %.3e" % (0, avg_nll))
 
-    avg_loss = 0.
-    for i in range(1, n_train + 1):
-        episode = episode_sampler['train'].get_episode()
-        loss = model.train(episode)
-        avg_loss += loss
+        avg_loss = 0.
+        best_val_nll = sys.float_info.max
+        for i in tqdm(range(1, n_train + 1)):
+            episode = episode_sampler['train'].get_episode()
+            loss = model.train(episode)
+            avg_loss += loss
 
-        if i % val_every_n == 0:
-            avg_nll = evaluate(model, episode_sampler['val'], n_val)
-            print("Iter: %d, val-nll: %.3e" % (i, avg_nll))
+            if i % print_every_n == 0:
+                print("Iter: %d, loss: %.3e" % (i, avg_loss / print_every_n))
+                avg_loss = 0.
 
-            if args.checkpt_dir != '':
-                model.save(args.checkpt_dir)
+            if i % val_every_n == 0:
+                avg_nll = evaluate(model, episode_sampler['val'], n_val)
+                print("Iter: %d, val-nll: %.3e" % (i, avg_nll))
 
-        if i % print_every_n == 0:
-            print("Iter: %d, loss: %.3e" % (i, avg_loss / print_every_n))
-            avg_loss = 0.
+                if save_best_val:
+                    if avg_nll < best_val_nll:
+                        best_val_nll = avg_nll
+                        print("=> Found winner on validation set: %.3e" % best_val_nll)
+                        if args.checkpt_dir != '':
+                            model.save(args.checkpt_dir)
+
+                        patience_iters = min(
+                            config['patience_iters'], patience_iters + 1)
+                    else:
+                        patience_iters -= 1
+                        print("=> Decreasing patience: %d" % patience_iters)
+                        if patience_iters == 0:
+                            print("=> Patience exhausted - ending training...")
+                            break
+                else:
+                    if args.checkpt_dir != '':
+                        model.save(args.checkpt_dir)
+
+        # Load best model
+        if args.checkpt_dir != '':
+            print("=> Loading winner on validation set")
+            model.recover_or_init(args.checkpt_dir)
 
     # Evaluate model after training on training, validation, and test sets
     avg_nll = evaluate(model, episode_sampler['train'], n_test)
@@ -106,24 +167,10 @@ def main():
     print("Test Avg NLL: %.3e" % (avg_nll))
 
     # Generate samples from trained model for test episodes
-    samples_dir = os.path.join(args.checkpt_dir, 'samples')
-    if not os.path.exists(samples_dir):
-        os.makedirs(samples_dir)
-
-    for i in range(n_samples):
-        curr_sample_dir = os.path.join(samples_dir, 'sample_%d' % i)
-        os.makedirs(curr_sample_dir)
-
-        episode = episode_sampler['test'].get_episode()
-        support_set = episode.support[0]
-        sample = model.sample(support_set, max_len)
-
-        for j in range(support_set.shape[0]):
-            write_seq(episode_sampler['test'].detokenize(support_set[j]),
-                      curr_sample_dir, 'support_%d' % j)
-
-        write_seq(episode_sampler['test'].detokenize(sample), curr_sample_dir,
-                  'model_sample')
+    if args.checkpt_dir != '':
+        samples_dir = os.path.join(args.checkpt_dir, 'samples')
+        write_samples(
+            model, episode_sampler['test'], samples_dir, n_samples, max_len)
 
 
 if __name__ == '__main__':
